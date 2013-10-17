2b5dc34671b3ec18f60ca33cb48b79c943ebb1ff
Roger Wilson <ctjinx@gmail.com>
2013-10-17 19:36:18 +0200

    Changed from spaces to tabs in Program.cs

--- FakeEvenCreator/Program.cs
+++ FakeEvenCreator/Program.cs
@@ -1,50 +1,49 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading;
 
 namespace KibanaTryout
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var indexer = new ElasticSearchIndexer(new ElasticSearchConfiguration());
-            
-            while (true)
-            {
-                var agg = new Aggregator();
-                agg.Blahonga();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var indexer = new ElasticSearchIndexer(new ElasticSearchConfiguration());
 
-                return;
+			while (true)
+			{
+				var agg = new Aggregator();
+				agg.Blahonga();
 
-                var timeStamp = DateTime.UtcNow.AddDays(new Random().Next(-10, 10));
+				return;
 
-                var eventToLog = EventToIndexCreator.CreateEventToIndex(timeStamp);
-                indexer.IndexLog(eventToLog);
+				var timeStamp = DateTime.UtcNow.AddDays(new Random().Next(-10, 10));
 
-                Console.WriteLine(eventToLog.JsonBody);
-                //Thread.Sleep(TimeSpan.FromSeconds(new Random().Next(1, 5)));
+				var eventToLog = EventToIndexCreator.CreateEventToIndex(timeStamp);
+				indexer.IndexLog(eventToLog);
 
-                //Start Datum - ID/paging - Poistion
-                //Hämta ett dygns data (med paging)
+				Console.WriteLine(eventToLog.JsonBody);
+				//Thread.Sleep(TimeSpan.FromSeconds(new Random().Next(1, 5)));
 
-                //Spara i dictionary - skapa nyckel
+				//Start Datum - ID/paging - Poistion
+				//Hämta ett dygns data (med paging)
 
-
-
-                //När allt är klart spara till elasticserch
+				//Spara i dictionary - skapa nyckel
 
 
-            }
-        }
-    }
 
-    public class AggressiveModel
-    {
-        public DateTime TimeStamp { get; set; }
-        public string Parameter { get; set; }
-        public int NumberOfUniqueUsers { get; set; }
-        //public List<string> UniqueUsers { get; set; }  
-    }
+				//När allt är klart spara till elasticserch
+
+
+			}
+		}
+	}
+
+	public class AggressiveModel
+	{
+		public DateTime TimeStamp { get; set; }
+		public string Parameter { get; set; }
+		public int NumberOfUniqueUsers { get; set; }
+		//public List<string> UniqueUsers { get; set; }  
+	}
 }

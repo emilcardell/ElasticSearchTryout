1eda6d8055d15f7346fa7928411605eca3c2d826
Roger Wilson <ctjinx@gmail.com>
2013-10-17 19:29:39 +0200

    Changed from spaces to tabs in ElasticSearchFields.cs

--- FakeEvenCreator/ElasticSearchFields.cs
+++ FakeEvenCreator/ElasticSearchFields.cs
@@ -1,11 +1,10 @@
 ﻿namespace KibanaTryout
 {
-    public class ElasticSearchFields
-    {
-        public const string Id = "_id";
-        public const string Timestamp = "@timestamp";
-        public const string Type = "@type";
-        public const string Message = "@message";
-
-    }
+	public class ElasticSearchFields
+	{
+		public const string Id = "_id";
+		public const string Timestamp = "@timestamp";
+		public const string Type = "@type";
+		public const string Message = "@message";
+	}
 }

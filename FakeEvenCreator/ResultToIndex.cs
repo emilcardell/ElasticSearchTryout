331d003236191ca1740704fa24bc2d53279f9e89
Roger Wilson <ctjinx@gmail.com>
2013-10-17 19:36:47 +0200

    Changed from spaces to tabs in ResultToIndex.cs

--- FakeEvenCreator/ResultToIndex.cs
+++ FakeEvenCreator/ResultToIndex.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace KibanaTryout
 {
-    public class ResultToIndex
-    {
-        public string Id { get; set; }
-        public string JsonBody { get; set; }
-        public string LogType { get; set; }
-        public DateTime TimeStamp { get; set; }
-        public string User { get; set; }
-        public List<string> Parameters { get; set; }
-    }
+	public class ResultToIndex
+	{
+		public string Id { get; set; }
+		public string JsonBody { get; set; }
+		public string LogType { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public string User { get; set; }
+		public List<string> Parameters { get; set; }
+	}
 }

6908c806ee8bff6c882de9769090fd5d1f24fcf4
Roger Wilson <ctjinx@gmail.com>
2013-10-17 19:37:27 +0200

    Changed from spaces to tabs in UserParameterEvent.cs

--- FakeEvenCreator/UserParameterEvent.cs
+++ FakeEvenCreator/UserParameterEvent.cs
@@ -4,45 +4,45 @@ using Newtonsoft.Json.Linq;
 
 namespace KibanaTryout
 {
-    public class UserParameterEvent
-    {
-       public static Dictionary<string, List<string>> userPerTermAndTime = new Dictionary<string, List<string>>();
-  
-       public List<ResultToIndex> CreateUserParameterEvent(ResultToIndex input)
-       {
-           var result = new List<ResultToIndex>();
-           foreach (var parameter in input.Parameters)
-           {
-               var output = new ResultToIndex();
-               output.Id = input.TimeStamp.ToString("yyyyMMddhh") + "_" + parameter;
+	public class UserParameterEvent
+	{
+		public static Dictionary<string, List<string>> userPerTermAndTime = new Dictionary<string, List<string>>();
 
-               var document = new JObject();
-               var intervalDate = new DateTime(input.TimeStamp.Year, input.TimeStamp.Month, input.TimeStamp.Day, input.TimeStamp.Hour, 0, 0);
-               document.Add(ElasticSearchFields.Timestamp, new JValue(intervalDate));
+		public List<ResultToIndex> CreateUserParameterEvent(ResultToIndex input)
+		{
+			var result = new List<ResultToIndex>();
+			foreach (var parameter in input.Parameters)
+			{
+				var output = new ResultToIndex();
+				output.Id = input.TimeStamp.ToString("yyyyMMddhh") + "_" + parameter;
 
-               if (!userPerTermAndTime.ContainsKey(output.Id))
-               {
-                   userPerTermAndTime.Add(output.Id, new List<string>());
-               }
+				var document = new JObject();
+				var intervalDate = new DateTime(input.TimeStamp.Year, input.TimeStamp.Month, input.TimeStamp.Day, input.TimeStamp.Hour, 0, 0);
+				document.Add(ElasticSearchFields.Timestamp, new JValue(intervalDate));
 
-               if (!userPerTermAndTime[output.Id].Contains(input.User))
-               {
-                   userPerTermAndTime[output.Id].Add(input.User);
-               }
+				if (!userPerTermAndTime.ContainsKey(output.Id))
+				{
+					userPerTermAndTime.Add(output.Id, new List<string>());
+				}
 
-               document.Add("Parameter", new JValue(parameter.Trim()));
-               document.Add("NumberOfUniqeUsers", new JValue(userPerTermAndTime[output.Id].Count));
-               document.Add(ElasticSearchFields.Type, new JValue("AGGRO"));
+				if (!userPerTermAndTime[output.Id].Contains(input.User))
+				{
+					userPerTermAndTime[output.Id].Add(input.User);
+				}
 
-               output.LogType = "AGGRO";
-               output.TimeStamp = intervalDate;
-               output.JsonBody = document.ToString(Newtonsoft.Json.Formatting.None);
+				document.Add("Parameter", new JValue(parameter.Trim()));
+				document.Add("NumberOfUniqeUsers", new JValue(userPerTermAndTime[output.Id].Count));
+				document.Add(ElasticSearchFields.Type, new JValue("AGGRO"));
 
-               result.Add(output);
-              
-           }
+				output.LogType = "AGGRO";
+				output.TimeStamp = intervalDate;
+				output.JsonBody = document.ToString(Newtonsoft.Json.Formatting.None);
 
-           return result;
-       }
-    }
+				result.Add(output);
+
+			}
+
+			return result;
+		}
+	}
 }
